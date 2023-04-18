5beb2a815b5e50d317b4a204d30ff8750a491b3e
lubod <lubo.daniel.dilov@gmail.com>
2023-04-18 19:44:59 +0300

    fixed image uploading

---
+++ WebApplication1/Migrations/20230309172818_init-lubo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarDeal.Migrations
+{
+    public partial class initlubo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
